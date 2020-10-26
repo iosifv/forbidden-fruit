831761488e2cad8e5eff330d58814eb3a89ec32b
Iosif Vee <iosifv@Iosifs-MacBook-Pro.local>
2020-10-26 22:53:09 +0000

    first set of casks saved

---
+++ 01-prerequisites.md
@@ -0,0 +1,8 @@
+Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+Prepare to install genesis repository
+brew install git
+brew install gpg
+
+
---
+++ 02-brew-mayhem.sh
@@ -0,0 +1,29 @@
+brew install cask
+
+# Usability and personal Casks
+brew cask install spotify
+brew cask install authy
+brew cask install slack
+brew cask install firefox
+brew cask install opera
+brew cask install franz
+brew cask install motrix
+
+# Programming Casks
+brew cask install phpstorm
+brew cask install pycharm-ce
+brew cask install visual-studio-code
+brew cask install postman
+brew cask install insomnia
+brew cask install iterm2
+
+# Programming brews
+brew install python
+brew install nvm
+
+# Utility brews
+brew install speedtest-cli
+brew install htop
+brew install watch
+
+
